3b7fb484497e778a55762fa2b3772469ee85c8c0
Nikhil B <nikhilb1208@gmail.com>
2021-05-01 15:56:57 -0500

    Add project files.

---
+++ MediatR_API_Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MediatR_API_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
