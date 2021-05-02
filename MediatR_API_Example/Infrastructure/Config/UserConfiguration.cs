ed468d250a61c766606bbc9a85d137d7421d7769
Nikhil B <nikhilb1208@gmail.com>
2021-05-01 19:13:08 -0500

    Added EntityFrame work and migration changes.

---
+++ MediatR_API_Example/Infrastructure/Config/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using MediatR_API_Example.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediatR_API_Example.Infrastructure.Config
+{
+    public class UserConfiguration
+         : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(25)
+                .IsRequired();
+            builder.Property(x => x.LastName)
+                .HasMaxLength(25)
+                .IsRequired();
+            builder.Property(x => x.SexId)
+                .IsRequired();
+        }
+    }
+}
---
+++ MediatR_API_Example/Infrastructure/ExampleDataContext.cs
@@ -0,0 +1,21 @@
+﻿using MediatR_API_Example.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MediatR_API_Example.Infrastructure
+{
+    public class ExampleDataContext : DbContext, IExampleDataContext
+    {
+        public ExampleDataContext(DbContextOptions<ExampleDataContext> options) : base(options)
+        { }
+        public DbSet<User> User { get; set; }
+        public DbSet<Sex> Sex { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ExampleDataContext).Assembly);
+        }
+    }
+}
